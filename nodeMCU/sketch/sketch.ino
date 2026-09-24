67f0e3df4f84def5e4d39427cee58de1f3c48ce5
Alexey Koshelev <Basher0303@yandex.ru>
2026-09-24 20:54:07 +0300

    [nodeMCU] Formatting

--- nodeMCU/sketch/sketch.ino
+++ nodeMCU/sketch/sketch.ino
@@ -9,148 +9,177 @@
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
 
+template <typename T>
+void consoleLogLn(T text);
+template <typename T>
+void consoleLog(T text);
 
-template < typename T > void consoleLogLn(T text);
-template < typename T > void consoleLog(T text);
-
-
-struct WiFiSettings {
-  char ssid[32];
-  char password[32];
+struct WiFiSettings
+{
+	char ssid[32];
+	char password[32];
 };
 
-class LED {
-  public:
-    LED(byte pin, byte enabledLevel) {
-      _pin = pin;
-      _enabledLevel = enabledLevel;
-      _disabledLevel = _enabledLevel == HIGH ? LOW : HIGH;
-      _timer = new GTimer(MS);
-      pinMode(pin, OUTPUT);
-      disable();
-    }
-    void enable() {
-      _state = true;
-      if(!_timer->isEnabled()) {
-        digitalWrite(_pin, _enabledLevel);
-      }
-    }
-    void disable() {
-      _state = false;
-      if(!_timer->isEnabled()) {
-        digitalWrite(_pin, _disabledLevel);
-      }
-    }
-    void startBlink(uint16_t prd) {
-      if(!_timer->isEnabled()) {
-        _blinkState = true;
-        _timer->setInterval(prd);
-      }
-    }
-    void stopBlink() {
-      _timer->stop();
-      digitalWrite(_pin, _state ? _enabledLevel : _disabledLevel);
-    }
-    bool blink() {
-      if(_timer->isReady()) {
-        _blinkState = !_blinkState;
-        digitalWrite(_pin, _blinkState ? _enabledLevel : _disabledLevel);
-      }
-    }
-  private:
-    byte _pin;
-    byte _enabledLevel;
-    byte _disabledLevel;
-    bool _enabled;
-    bool _state;
-    bool _blinkState;
-    GTimer* _timer;
-};
-
-
-class WiFiConnect {
+class LED
+{
 public:
-  WiFiConnect(WiFiSettings &wifiSettings, LED &led) : _wifiSettings(wifiSettings), _led(led) {
-    _timerConnected = new GTimer(MS);
-  }
+	LED(byte pin, byte enabledLevel)
+	{
+		_pin = pin;
+		_enabledLevel = enabledLevel;
+		_disabledLevel = _enabledLevel == HIGH ? LOW : HIGH;
+		_timer = new GTimer(MS);
+		pinMode(pin, OUTPUT);
+		disable();
+	}
+	void enable()
+	{
+		_state = true;
+		if (!_timer->isEnabled())
+		{
+			digitalWrite(_pin, _enabledLevel);
+		}
+	}
+	void disable()
+	{
+		_state = false;
+		if (!_timer->isEnabled())
+		{
+			digitalWrite(_pin, _disabledLevel);
+		}
+	}
+	void startBlink(uint16_t prd)
+	{
+		if (!_timer->isEnabled())
+		{
+			_blinkState = true;
+			_timer->setInterval(prd);
+		}
+	}
+	void stopBlink()
+	{
+		_timer->stop();
+		digitalWrite(_pin, _state ? _enabledLevel : _disabledLevel);
+	}
+	bool blink()
+	{
+		if (_timer->isReady())
+		{
+			_blinkState = !_blinkState;
+			digitalWrite(_pin, _blinkState ? _enabledLevel : _disabledLevel);
+		}
+	}
 
-  void begin() {
-    WiFi.begin(_wifiSettings.ssid, _wifiSettings.password);
-    _timerConnected->setInterval(500);
-  }
-
-  void stop() {
-    _timerConnected->stop();
-    _led.stopBlink();
-  }
-
-  void checkConnect() {
-    if(_timerConnected->isReady()) {
-      if(WiFi.status() == WL_CONNECTED) {
-        _led.stopBlink();
-        
-        // consoleLogLn("WiFi connected");
-        // consoleLogLn("IP address: ");
-        // consoleLogLn(WiFi.localIP());
-      } else {
-        _led.startBlink(500);
-
-        consoleLogLn("WiFi not connected :(");
-        consoleLog("SSID = ");
-        consoleLogLn(_wifiSettings.ssid);
-        consoleLog("Password = ");
-        consoleLogLn(_wifiSettings.password);
-      }
-    }
-  }
 private:
-  WiFiSettings &_wifiSettings;
-  LED &_led;
-  GTimer* _timerConnected;
+	byte _pin;
+	byte _enabledLevel;
+	byte _disabledLevel;
+	bool _enabled;
+	bool _state;
+	bool _blinkState;
+	GTimer *_timer;
 };
 
-class WebServer {
-public: 
-  WebServer(char* ssid, char* password, int port, WiFiSettings &wifiSettings)
-    : _ssid(ssid), _password(password), _port(port), _wifiSettings(wifiSettings) {
-    _server = new ESP8266WebServer(port);
-    _localIp = IPAddress(192,168,1,1);
-    _gateway = IPAddress(192,168,1,1);
-    _subnet = IPAddress(255,255,255,0);
-  }
+class WiFiConnect
+{
+public:
+	WiFiConnect(WiFiSettings &wifiSettings, LED &led) : _wifiSettings(wifiSettings), _led(led)
+	{
+		_timerConnected = new GTimer(MS);
+	}
 
-  void begin() {
-    WiFi.softAP(_ssid, _password);
-    consoleLogLn("Server start!");
-    WiFi.softAPConfig(_localIp, _gateway, _subnet);
-    delay(100);
-    _server->on("/", std::bind(&WebServer::_handleGetMainPage, this));
-    _server->on("/settings", HTTP_POST, std::bind(&WebServer::_handlePostSettings, this));
+	void begin()
+	{
+		WiFi.begin(_wifiSettings.ssid, _wifiSettings.password);
+		_timerConnected->setInterval(500);
+	}
 
-   _server->begin();
-  }
+	void stop()
+	{
+		_timerConnected->stop();
+		_led.stopBlink();
+	}
 
-  void stop() {
-    WiFi.softAPdisconnect();
-    _server->stop();
-  }
+	void checkConnect()
+	{
+		if (_timerConnected->isReady())
+		{
+			if (WiFi.status() == WL_CONNECTED)
+			{
+				_led.stopBlink();
 
-   void handleClient() {
-    _server->handleClient();
-  }
-private: 
-  char* _ssid;
-  char* _password;
-  int _port;
-  WiFiSettings &_wifiSettings;
-  
-  ESP8266WebServer* _server;
-  IPAddress _localIp;
-  IPAddress _gateway;
-  IPAddress _subnet;
+				// consoleLogLn("WiFi connected");
+				// consoleLogLn("IP address: ");
+				// consoleLogLn(WiFi.localIP());
+			}
+			else
+			{
+				_led.startBlink(500);
 
-  void _handleGetMainPage() {
-    String html = R"rawliteral(
+				consoleLogLn("WiFi not connected :(");
+				consoleLog("SSID = ");
+				consoleLogLn(_wifiSettings.ssid);
+				consoleLog("Password = ");
+				consoleLogLn(_wifiSettings.password);
+			}
+		}
+	}
+
+private:
+	WiFiSettings &_wifiSettings;
+	LED &_led;
+	GTimer *_timerConnected;
+};
+
+class WebServer
+{
+public:
+	WebServer(char *ssid, char *password, int port, WiFiSettings &wifiSettings)
+		: _ssid(ssid), _password(password), _port(port), _wifiSettings(wifiSettings)
+	{
+		_server = new ESP8266WebServer(port);
+		_localIp = IPAddress(192, 168, 1, 1);
+		_gateway = IPAddress(192, 168, 1, 1);
+		_subnet = IPAddress(255, 255, 255, 0);
+	}
+
+	void begin()
+	{
+		WiFi.softAP(_ssid, _password);
+		consoleLogLn("Server start!");
+		WiFi.softAPConfig(_localIp, _gateway, _subnet);
+		delay(100);
+		_server->on("/", std::bind(&WebServer::_handleGetMainPage, this));
+		_server->on("/settings", HTTP_POST, std::bind(&WebServer::_handlePostSettings, this));
+
+		_server->begin();
+	}
+
+	void stop()
+	{
+		WiFi.softAPdisconnect();
+		_server->stop();
+	}
+
+	void handleClient()
+	{
+		_server->handleClient();
+	}
+
+private:
+	char *_ssid;
+	char *_password;
+	int _port;
+	WiFiSettings &_wifiSettings;
+
+	ESP8266WebServer *_server;
+	IPAddress _localIp;
+	IPAddress _gateway;
+	IPAddress _subnet;
+
+	void _handleGetMainPage()
+	{
+		String html = R"rawliteral(
         <!DOCTYPE html>
         <html lang="ru">
         <head>
@@ -250,87 +279,105 @@ private:
         </html>
     )rawliteral";
 
-    html.replace("%SSID%", _wifiSettings.ssid);
-    html.replace("%PASSWORD%", _wifiSettings.password);
-    _server->send(200, "text/html", html); // Отправляем HTML-форму
-  }
+		html.replace("%SSID%", _wifiSettings.ssid);
+		html.replace("%PASSWORD%", _wifiSettings.password);
+		_server->send(200, "text/html", html); // Отправляем HTML-форму
+	}
 
-  void _handlePostSettings() {
-    if (_server->hasArg("ssid")) { 
-      String ssid = _server->arg("ssid");
-      String password = _server->arg("password");
-      ssid.toCharArray(_wifiSettings.ssid, sizeof(_wifiSettings.ssid));
-      password.toCharArray(_wifiSettings.password, sizeof(_wifiSettings.password));
-      EEPROM.put(0, _wifiSettings); 
-      EEPROM.commit();
+	void _handlePostSettings()
+	{
+		if (_server->hasArg("ssid"))
+		{
+			String ssid = _server->arg("ssid");
+			String password = _server->arg("password");
+			ssid.toCharArray(_wifiSettings.ssid, sizeof(_wifiSettings.ssid));
+			password.toCharArray(_wifiSettings.password, sizeof(_wifiSettings.password));
+			EEPROM.put(0, _wifiSettings);
+			EEPROM.commit();
 
-      _server->send(200, "text/plain", "OK"); // Отправляем ответ
-    } else {
-      _server->send(400, "text/plain", "Error");
-    }
-  }
+			_server->send(200, "text/plain", "OK"); // Отправляем ответ
+		}
+		else
+		{
+			_server->send(400, "text/plain", "Error");
+		}
+	}
 };
 
-class SensorBuffer {
-public: 
-  SensorBuffer(int size) {
-    _size = size;
-    _buffer = new float[size];
-  }
+class SensorBuffer
+{
+public:
+	SensorBuffer(int size)
+	{
+		_size = size;
+		_buffer = new float[size];
+	}
 
-  void push(float value) {
-    if(!isFilled()) {
-      _buffer[_currentIndex] = value;
-      _currentIndex += 1;
-    }
-  }
+	void push(float value)
+	{
+		if (!isFilled())
+		{
+			_buffer[_currentIndex] = value;
+			_currentIndex += 1;
+		}
+	}
 
-  void pushLastValue() {
-    if(!isFilled()) {
-      if(_currentIndex == 0) {
-        _buffer[_currentIndex] = 0;
-      } else {
-        _buffer[_currentIndex] = _buffer[_currentIndex - 1];
-      }
-      _currentIndex += 1;
-    }
-  }
+	void pushLastValue()
+	{
+		if (!isFilled())
+		{
+			if (_currentIndex == 0)
+			{
+				_buffer[_currentIndex] = 0;
+			}
+			else
+			{
+				_buffer[_currentIndex] = _buffer[_currentIndex - 1];
+			}
+			_currentIndex += 1;
+		}
+	}
 
-  float getAverage() {
-    if (_size == 0) {
-        return 0;
-    }
+	float getAverage()
+	{
+		if (_size == 0)
+		{
+			return 0;
+		}
 
-    float sum = 0;
-    for (int i = 0; i < _size; i++) {
-        sum += _buffer[i];
-    }
-    return static_cast<float>(sum) / _size;
-  }
+		float sum = 0;
+		for (int i = 0; i < _size; i++)
+		{
+			sum += _buffer[i];
+		}
+		return static_cast<float>(sum) / _size;
+	}
 
-  bool isFilled() {
-    return _currentIndex == _size;
-  }
+	bool isFilled()
+	{
+		return _currentIndex == _size;
+	}
 
-  void clear() {
-    for (int i = 0; i < _size; i++) {
-      _buffer[i] = 0;
-    }
-    _currentIndex = 0;
-  }
+	void clear()
+	{
+		for (int i = 0; i < _size; i++)
+		{
+			_buffer[i] = 0;
+		}
+		_currentIndex = 0;
+	}
 
 private:
-  int _size;
-  int _currentIndex = 0;
-  float* _buffer;
+	int _size;
+	int _currentIndex = 0;
+	float *_buffer;
 };
 
+#define BTN_PIN D7 // Пин, к которому подключена кнопка
+#define LED_PIN D4 // Встроенный светодиод
 
-#define BTN_PIN D7  // Пин, к которому подключена кнопка
-#define LED_PIN D4  // Встроенный светодиод
-
-#define READ_SENSORS_INTERVAL  2000
-#define SEND_REQUEST_INTERVAL  10000
+#define READ_SENSORS_INTERVAL 2000
+#define SEND_REQUEST_INTERVAL 10000
 
 SensorBuffer co2Buffer(SEND_REQUEST_INTERVAL / READ_SENSORS_INTERVAL);
 SensorBuffer temperatureBuffer(SEND_REQUEST_INTERVAL / READ_SENSORS_INTERVAL);
@@ -343,7 +390,7 @@ GTimer timeoutSensorsReady(MS);
 
 GButton button(BTN_PIN);
 LED led(LED_PIN, LOW);
-String urlRequest =  "http://api.alex-basher.ru";
+String urlRequest = "http://api.alex-basher.ru";
 GyverBME280 bme;
 WiFiSettings wifiSettings;
 WiFiConnect wifiConnect(wifiSettings, led);
@@ -352,154 +399,179 @@ bool isSensorsReady = false;
 bool settingsMode = false;
 bool debugMode = true;
 
+void setup()
+{
+	Serial.begin(115200);
+	co2Serial.begin(9600);
 
-void setup() {
-  Serial.begin(115200);
-  co2Serial.begin(9600);
+	EEPROM.begin(sizeof(wifiSettings.ssid) + sizeof(wifiSettings.password));
+	EEPROM.get(0, wifiSettings);
 
-  EEPROM.begin(sizeof(wifiSettings.ssid) + sizeof(wifiSettings.password));
-  EEPROM.get(0, wifiSettings);
+	pinMode(BTN_PIN, INPUT_PULLUP); // Включаем встроенный pull-up
+	button.setTickMode(AUTO);
 
-  pinMode(BTN_PIN, INPUT_PULLUP);  // Включаем встроенный pull-up
-  button.setTickMode(AUTO);
-
-  wifiConnect.begin();
-  timeoutSensorsReady.setTimeout(10 * 1000);
-  if (!bme.begin(0x76)) consoleLogLn("Bme error!");
+	wifiConnect.begin();
+	timeoutSensorsReady.setTimeout(10 * 1000);
+	if (!bme.begin(0x76))
+		consoleLogLn("Bme error!");
 }
 
-void loop() {
-  buttonControll();
-  webServer.handleClient();
-  led.blink();
-  wifiConnect.checkConnect();
+void loop()
+{
+	buttonControll();
+	webServer.handleClient();
+	led.blink();
+	wifiConnect.checkConnect();
 
-  if(timeoutSensorsReady.isReady()) {
-    isSensorsReady = true;
-  } 
-  if(isSensorsReady && timerReadSensors.isReady()) {
-    int co2 = readCO2();
-    if (co2 != -1) {
-      co2Buffer.push(co2);
-    } else {
-      co2Buffer.pushLastValue();
-      Serial.println("Ошибка чтения CO2!");
-    }
+	if (timeoutSensorsReady.isReady())
+	{
+		isSensorsReady = true;
+	}
+	if (isSensorsReady && timerReadSensors.isReady())
+	{
+		int co2 = readCO2();
+		if (co2 != -1)
+		{
+			co2Buffer.push(co2);
+		}
+		else
+		{
+			co2Buffer.pushLastValue();
+			Serial.println("Ошибка чтения CO2!");
+		}
 
-    temperatureBuffer.push(bme.readTemperature());
-    humidityBuffer.push(bme.readHumidity());
-    pressureBuffer.push(bme.readPressure());
+		temperatureBuffer.push(bme.readTemperature());
+		humidityBuffer.push(bme.readHumidity());
+		pressureBuffer.push(bme.readPressure());
 
-    if(co2Buffer.isFilled()) {
-      Serial.print("CO2: ");
-      Serial.println(co2Buffer.getAverage());
+		if (co2Buffer.isFilled())
+		{
+			Serial.print("CO2: ");
+			Serial.println(co2Buffer.getAverage());
 
-      Serial.print("Temperature: ");
-      Serial.println(temperatureBuffer.getAverage());
+			Serial.print("Temperature: ");
+			Serial.println(temperatureBuffer.getAverage());
 
-      Serial.print("Humidity: ");
-      Serial.println(humidityBuffer.getAverage());
+			Serial.print("Humidity: ");
+			Serial.println(humidityBuffer.getAverage());
 
-      Serial.print("Pressure: ");
-      Serial.println(pressureBuffer.getAverage());
+			Serial.print("Pressure: ");
+			Serial.println(pressureBuffer.getAverage());
 
-      sendHttpRequest();
+			sendHttpRequest();
 
-      co2Buffer.clear();
-      temperatureBuffer.clear();
-      humidityBuffer.clear();
-      pressureBuffer.clear();
-    }
-  }
+			co2Buffer.clear();
+			temperatureBuffer.clear();
+			humidityBuffer.clear();
+			pressureBuffer.clear();
+		}
+	}
 }
 
-int readCO2() {
-  byte requestData[9] = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 }; // Команда для чтения CO2
-  unsigned char responseData[9]; // Буфер для получения данных
+int readCO2()
+{
+	byte requestData[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}; // Команда для чтения CO2
+	unsigned char responseData[9];												  // Буфер для получения данных
 
-  // Отправляем команду датчику
-  co2Serial.write(requestData, 9);
-  memset(responseData, 0, 9);
-  delay(50);
+	// Отправляем команду датчику
+	co2Serial.write(requestData, 9);
+	memset(responseData, 0, 9);
+	delay(50);
 
-  co2Serial.readBytes(responseData, 9);
-  // Проверка контрольной суммы
-  byte checksum = 0;
-  for (int i = 1; i < 8; i++) {
-    checksum += responseData[i];
-  }
-  checksum = 255 - checksum + 1;
+	co2Serial.readBytes(responseData, 9);
+	// Проверка контрольной суммы
+	byte checksum = 0;
+	for (int i = 1; i < 8; i++)
+	{
+		checksum += responseData[i];
+	}
+	checksum = 255 - checksum + 1;
 
-  if (responseData[8] == checksum) {
-    unsigned int HLconcentration = (unsigned int) responseData[2]; 
-    unsigned int LLconcentration = (unsigned int) responseData[3]; 
-    unsigned int co2 = (256 * HLconcentration) + LLconcentration;
-    return co2;
-  } else {
-    Serial.println("Контрольная сумма не совпала!");
-    return -1; // Ошибка контрольной суммы
-  }
+	if (responseData[8] == checksum)
+	{
+		unsigned int HLconcentration = (unsigned int)responseData[2];
+		unsigned int LLconcentration = (unsigned int)responseData[3];
+		unsigned int co2 = (256 * HLconcentration) + LLconcentration;
+		return co2;
+	}
+	else
+	{
+		Serial.println("Контрольная сумма не совпала!");
+		return -1; // Ошибка контрольной суммы
+	}
 }
 
-void buttonControll() {
-  if (button.isHolded()) {
-    settingsMode = !settingsMode;
+void buttonControll()
+{
+	if (button.isHolded())
+	{
+		settingsMode = !settingsMode;
 
-
-    if(settingsMode) {
-      wifiConnect.stop();
-      webServer.begin();
-      led.enable();
-    } else {
-      webServer.stop();
-      wifiConnect.begin();
-      led.disable();
-    }
-  }
-  if (button.isTriple()) {
-    debugMode = !debugMode;
-    consoleLogLn("Button triple");
-  }
+		if (settingsMode)
+		{
+			wifiConnect.stop();
+			webServer.begin();
+			led.enable();
+		}
+		else
+		{
+			webServer.stop();
+			wifiConnect.begin();
+			led.disable();
+		}
+	}
+	if (button.isTriple())
+	{
+		debugMode = !debugMode;
+		consoleLogLn("Button triple");
+	}
 }
 
-void sendHttpRequest() {
-  String jsonData;
-  StaticJsonDocument<200> jsonDoc;
-  jsonDoc["co2"] = co2Buffer.getAverage();
-  jsonDoc["temperature"] = temperatureBuffer.getAverage();
-  jsonDoc["humidity"] = humidityBuffer.getAverage();
-  jsonDoc["pressure"] = pressureBuffer.getAverage();
-  serializeJson(jsonDoc, jsonData);
-  
-  // WiFiClientSecure client;
-  // Ignore SSL certificate validation
-  // client.setInsecure();
+void sendHttpRequest()
+{
+	String jsonData;
+	StaticJsonDocument<200> jsonDoc;
+	jsonDoc["co2"] = co2Buffer.getAverage();
+	jsonDoc["temperature"] = temperatureBuffer.getAverage();
+	jsonDoc["humidity"] = humidityBuffer.getAverage();
+	jsonDoc["pressure"] = pressureBuffer.getAverage();
+	serializeJson(jsonDoc, jsonData);
 
-  WiFiClient client;
-  
-  //create an HTTPClient instance
-  HTTPClient https;
+	// WiFiClientSecure client;
+	// Ignore SSL certificate validation
+	// client.setInsecure();
 
-  https.begin(client, urlRequest + "/set");      
-  https.addHeader("Content-Type", "application/json"); 
+	WiFiClient client;
 
-  int httpCode = https.POST(jsonData);
-  String payload = https.getString();
+	// create an HTTPClient instance
+	HTTPClient https;
 
-  Serial.println(httpCode);   //Распечатать код возврата HTTP
-  Serial.println(payload);    //Полезная нагрузка для ответа на запрос печати
+	https.begin(client, urlRequest + "/set");
+	https.addHeader("Content-Type", "application/json");
 
-  https.end();  //Закрыть соединение
+	int httpCode = https.POST(jsonData);
+	String payload = https.getString();
+
+	Serial.println(httpCode); // Распечатать код возврата HTTP
+	Serial.println(payload);  // Полезная нагрузка для ответа на запрос печати
+
+	https.end(); // Закрыть соединение
 }
 
-template < typename T > void consoleLogLn(T text) {
-  if(debugMode) {
-    Serial.println(text);
-  }
+template <typename T>
+void consoleLogLn(T text)
+{
+	if (debugMode)
+	{
+		Serial.println(text);
+	}
 }
 
-template < typename T > void consoleLog(T text) {
-  if(debugMode) {
-    Serial.print(text);
-  }
+template <typename T>
+void consoleLog(T text)
+{
+	if (debugMode)
+	{
+		Serial.print(text);
+	}
 }
